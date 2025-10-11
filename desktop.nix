cda3d5a0e77598ca0ae2aad762ceaa24121e2497
sayedmurtaza24 <sayedmurtazamuttahary@gmail.com>
2025-10-11 22:48:33 +0200

    chatgpt as desktop entry

--- desktop.nix
+++ desktop.nix
@@ -1,10 +1,19 @@
 {
-  xdg.desktopEntries.chatgpt = {
-    name = "ChatGPT";
-    genericName = "ChatGPT App";
-    exec = "flatpak run com.brave.Browser --app=https://chat.openai.com";
-    terminal = false;
-    icon = "com.brave.Browser";
-    categories = [ "Network" "WebBrowser" ];
-  };
+  pkgs,
+  ...
+}:
+{
+xdg.desktopEntries.chatgpt = {
+  name = "ChatGPT";
+  genericName = "AI Assistant";
+  comment = "Chat with ChatGPT";
+  exec = "chromium --app=https://chat.openai.com";
+  icon = "${pkgs.fetchurl {
+    url = "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg";
+    sha256 = "sha256-Wo2tKGTMHIEJ6650vqRH3y8wuXi9rVZ0kkfEBuHLIic=";
+  }}";
+  categories = [ "Utility" "Chat" ];
+  type = "Application";
+  terminal = false;
+};
 }
