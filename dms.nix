3afc6e548421583013fe445495c4b2505d3b1c28
sayedmurtaza24 <sayedmurtazamuttahary@gmail.com>
2025-11-21 00:00:54 +0100

    dms update

--- dms.nix
+++ dms.nix
@@ -3,14 +3,17 @@
     enable = true;
     systemd = {
       enable = true;
+      restartIfChanged = true;
     };
     enableSystemMonitoring = true;
     enableClipboard = true;
     enableVPN = true;
-    enableBrightnessControl = false;
+    enableBrightnessControl = true;
+    enableColorPicker = true;
     enableDynamicTheming = true;
     enableAudioWavelength = true;
     enableCalendarEvents = true;
+    enableSystemSound = true;
   };
 
   wayland.windowManager.hyprland.settings = {
