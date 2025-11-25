921511403b3bbd7b6356d01cf8d07711424339a9
sayedmurtaza24 <sayedmurtazamuttahary@gmail.com>
2025-11-25 23:13:04 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -3,18 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
-
     nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    caelestia-shell = {
-      url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -23,19 +15,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia-shell = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
-    };
-
     dgop = {
       url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dms-cli = {
-      url = "github:AvengeMedia/danklinux";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -43,12 +24,6 @@
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # vicinae = {
-    #   url = "github:vicinaehq/vicinae/v0.16.0";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    #   inputs.flake-utils.follows = "flake-utils";
-    # };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
@@ -57,11 +32,8 @@
         pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
         modules = [
           ./home.nix
-          inputs.caelestia-shell.homeManagerModules.default
-          inputs.noctalia-shell.homeModules.default
           inputs.dank-material-shell.homeModules.dankMaterialShell.default
           inputs.nix-flatpak.homeManagerModules.nix-flatpak
-          # inputs.vicinae.homeManagerModules.default
         ];
         extraSpecialArgs = { inherit inputs; };
       };
