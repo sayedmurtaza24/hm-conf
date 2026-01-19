c0198533ff34d81e4d1b7b359c4d111d6e1489a9
murtaza <sayedmurtazamuttahary@gmail.com>
2026-01-19 10:08:54 +0100

    fix ghostty on mac

--- ghostty.nix
+++ ghostty.nix
@@ -1,68 +1,78 @@
-{ pkgs, ... }:{
+{ pkgs, lib, ... }:{
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
     systemd.enable = !pkgs.stdenv.hostPlatform.isDarwin;
     package = pkgs.ghostty-bin;
 
-    settings = {
-      # mac only
-      background-blur = if pkgs.stdenv.hostPlatform.isDarwin then 90 else 0;
-      macos-titlebar-style = "hidden";
-      # end
+    settings = lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isDarwin {
+        background-blur = 60;
+        macos-titlebar-style = "hidden";
+        adjust-cell-height = 2;
+        adjust-font-baseline = 1;
+        adjust-cursor-height = 2;
+        font-thicken = true;
+        font-thicken-strength = 1;
+        alpha-blending = "native";
+        font-synthetic-style = "bold,italic,bold-italic";
+        font-size = 18;
+      })
 
-      font-family = "Operator Mono";
-      font-style = "Book";
-      font-style-italic = "Book Italic";
-      font-style-bold = "Medium";
-      font-style-bold-italic = "Medium Italic";
-      adjust-cell-height = 2;
-      adjust-font-baseline = 1;
-      adjust-cursor-height = 2;
+      {
+        font-family = "Operator Mono";
+        font-style = "Book";
+        font-style-italic = "Book Italic";
+        font-style-bold = "Medium";
+        font-style-bold-italic = "Medium Italic";
+        adjust-cell-height = 2;
+        adjust-font-baseline = 1;
+        adjust-cursor-height = 2;
 
-      font-thicken = true;
-      font-thicken-strength = 1;
-      alpha-blending = "native";
+        font-thicken = true;
+        font-thicken-strength = 1;
+        alpha-blending = "native";
 
-      font-size = 14;
+        font-size = if pkgs.stdenv.hostPlatform.isDarwin then 16 else 14;
 
-      theme = "Kanagawabones";
-      # config-file = "${config.xdg.configHome}/ghostty/config-dankcolors";
-      background = "#141416";
-      minimum-contrast = 1;
-      cursor-style = "block";
+        theme = "Kanagawabones";
+        # config-file = "${config.xdg.configHome}/ghostty/config-dankcolors";
+        background = "#141416";
+        minimum-contrast = 1;
+        cursor-style = "block";
 
-      cursor-click-to-move = true;
-      background-opacity = 0.80;
-      unfocused-split-opacity = 0.7;
+        cursor-click-to-move = true;
+        background-opacity = 0.80;
+        unfocused-split-opacity = 0.7;
 
-      scrollback-limit = 10000000;
-      link-url = true;
-      fullscreen = false;
+        scrollback-limit = 10000000;
+        link-url = true;
+        fullscreen = false;
 
-      window-padding-x = 10;
-      window-padding-y = 10;
-      window-padding-balance = true;
-      window-padding-color = "background";
-      window-vsync = true;
-      window-inherit-working-directory = true;
-      window-inherit-font-size = false;
-      window-decoration = "auto";
-      window-show-tab-bar = "never";
+        window-padding-x = 10;
+        window-padding-y = 10;
+        window-padding-balance = true;
+        window-padding-color = "background";
+        window-vsync = true;
+        window-inherit-working-directory = true;
+        window-inherit-font-size = false;
+        window-decoration = "auto";
+        window-show-tab-bar = "never";
 
-      focus-follows-mouse = true;
+        focus-follows-mouse = true;
 
-      copy-on-select = "clipboard";
+        copy-on-select = "clipboard";
 
-      keybind = [
-        "ctrl+\\=new_split:right"
-        "ctrl+-=new_split:down"
-        "ctrl+shift+k=goto_split:up"
-        "ctrl+shift+j=goto_split:down"
-        "ctrl+shift+h=goto_split:left"
-        "ctrl+shift+l=goto_split:right"
-        "ctrl+f=toggle_split_zoom"
-      ];
-    };
+        keybind = [
+          "ctrl+\\=new_split:right"
+          "ctrl+-=new_split:down"
+          "ctrl+shift+k=goto_split:up"
+          "ctrl+shift+j=goto_split:down"
+          "ctrl+shift+h=goto_split:left"
+          "ctrl+shift+l=goto_split:right"
+          "ctrl+f=toggle_split_zoom"
+        ];
+      }
+    ];
   };
 }
