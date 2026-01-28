a4c38637ff982507a3bffabdb2462274a74f0e99
murtaza <sayedmurtazamuttahary@gmail.com>
2026-01-28 20:12:37 +0100

    add .env to direnv config

--- modules/mise-direnv.nix
+++ modules/mise-direnv.nix
@@ -5,6 +5,10 @@
 
   programs.direnv = {
     enable = true;
+    config = {
+      load_dotenv = true;
+      strict_env = true;
+    };
     mise.enable = true;
     nix-direnv.enable = true;
   };
